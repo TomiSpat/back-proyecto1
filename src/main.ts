bbc87c7b4fb5442ffdf4e3001bb866bce7915c47
Tomas <105675729+TomiSpat@users.noreply.github.com>
2025-09-05 10:47:55 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://front-proyecto1.vercel.app",
-    credentials: true
+    origin: "https://front-proyecto1.vercel.app"
   });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
